0f286fba83c031498e3d5652086142367a712a5c
Piotr Dobiech <pitcer@protonmail.com>
2019-09-08 22:50:07 +0200

    Setup project for publishing in bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+
 plugins {
 	`java-library`
+	`maven-publish`
+	id("com.jfrog.bintray") version "1.8.4" apply false
 }
 
 subprojects {
 	apply {
 		plugin("java-library")
+		plugin("maven-publish")
+		plugin("com.jfrog.bintray")
 	}
 
 	repositories {
@@ -27,5 +33,37 @@ subprojects {
 		test {
 			useJUnitPlatform()
 		}
+	}
+
+	task<Jar>("sourcesJar") {
+		from(sourceSets["main"].allSource)
+		archiveClassifier.set("sources")
+	}
+
+	publishing {
+		publications {
+			create<MavenPublication>("maven") {
+				from(components["java"])
+				artifact(tasks["sourcesJar"])
+			}
+		}
+	}
+
+	configure<BintrayExtension> {
+		user = properties["bintray-user"] as String?
+		key = properties["bintray-api-key"] as String?
+		publish = true
+		setPublications("maven")
+		pkg(closureOf<BintrayExtension.PackageConfig> {
+			repo = properties["mcparkour-bintray-repository"] as String?
+			userOrg = properties["mcparkour-bintray-organization"] as String?
+			name = project.name
+			desc = project.description
+			websiteUrl = "https://github.com/mcparkournet/common"
+			issueTrackerUrl = "$websiteUrl/issues"
+			vcsUrl = "$websiteUrl.git"
+			setLicenses("MIT")
+			setLabels("java", "common", "utils", "utilities")
+		})
 	}
 }
